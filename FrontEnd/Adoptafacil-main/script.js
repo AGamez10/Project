e4328289573d462da17967ad5dd1c243bc074de3
AGamez10 <00andresgamez@gmail.com>
2025-03-25 08:33:22 -0500

    Unificacion de todos los repositorios en uno.

---
+++ FrontEnd/Adoptafacil-main/script.js
@@ -0,0 +1,22 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('Partículas cargadas');
+});
+
+const nombre = document.getElementById('nombre');
+const correo = document.getElementById('correo');
+
+nombre.addEventListener('input', function() {
+    if (nombre.value.length < 3) {
+        nombre.setCustomValidity('El nombre debe tener al menos 3 caracteres.');
+    } else {
+        nombre.setCustomValidity('');
+    }
+});
+
+correo.addEventListener('input', function() {
+    if (!correo.value.includes('@')) {
+        correo.setCustomValidity('Ingrese un correo electrónico válido.');
+    } else {
+        correo.setCustomValidity('');
+    }
+});
